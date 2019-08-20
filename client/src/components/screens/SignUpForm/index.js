2961ec386661fd870827108406e74637320352ab
David Grosh <dovidgrosh98@gmail.com>
2019-08-20 17:29:08 -0400

    added err msg

--- client/src/components/screens/SignUpForm/index.js
+++ client/src/components/screens/SignUpForm/index.js
@@ -52,66 +52,67 @@ class SignUpForm extends React.Component {
     if (showError) {
       errMessage = (
         <div>
-          <span>an error occured plz try again</span>
+          <span>couldn't register account try new username</span>
         </div>
       )
     }
     return (
       <Fragment>
-      {errMessage}
-      <form className='form' onSubmit={this.handleSubmitForm}>
-        <div>
-          <label htmlFor='name'>Name</label>
-          <input
-            required
-            type='text'
-            name='name'
-            onChange={this.handleTextInput}
-            value={this.state.name}
-          />
-        </div>
-        <div>
-          <label htmlFor='username'>Username</label>
-          <input
-            required
-            type='text'
-            name='username'
-            onChange={this.handleTextInput}
-            value={this.state.username}
-          />
-        </div>
-        <div>
-          <label htmlFor='password'>Password</label>
-          <input
-            required
-            type='password'
-            name='password'
-            onChange={this.handleTextInput}
-            value={this.state.password}
-          />
-        </div>
-        <div>
-          <label htmlFor='email'>Email</label>
-          <input
-            required
-            type='text'
-            name='email'
-            onChange={this.handleTextInput}
-            value={this.state.email}
-          />
-        </div>
-        <div>
-          <label htmlFor='linkedin'>Linkedin</label>
-          <input
-            required
-            type='text'
-            name='linkedin'
-            onChange={this.handleTextInput}
-            value={this.state.linkedin}
-          />
-        </div>
-        <button>Sign Up</button>
-      </form>
+        <h2>Signup</h2>
+        {errMessage}
+        <form className='form' onSubmit={this.handleSubmitForm}>
+          <div>
+            <label htmlFor='name'>Name</label>
+            <input
+              required
+              type='text'
+              name='name'
+              onChange={this.handleTextInput}
+              value={this.state.name}
+            />
+          </div>
+          <div>
+            <label htmlFor='username'>Username</label>
+            <input
+              required
+              type='text'
+              name='username'
+              onChange={this.handleTextInput}
+              value={this.state.username}
+            />
+          </div>
+          <div>
+            <label htmlFor='password'>Password</label>
+            <input
+              required
+              type='password'
+              name='password'
+              onChange={this.handleTextInput}
+              value={this.state.password}
+            />
+          </div>
+          <div>
+            <label htmlFor='email'>Email</label>
+            <input
+              required
+              type='text'
+              name='email'
+              onChange={this.handleTextInput}
+              value={this.state.email}
+            />
+          </div>
+          <div>
+            <label htmlFor='linkedin'>Linkedin</label>
+            <input
+              required
+              type='text'
+              name='linkedin'
+              onChange={this.handleTextInput}
+              value={this.state.linkedin}
+            />
+          </div>
+          <button>Sign Up</button>
+        </form>
       </Fragment>
     )
   }
