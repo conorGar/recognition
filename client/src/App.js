4053d330c711f91f91faa48bd91d255044c96fa9
David Grosh <dovidgrosh98@gmail.com>
2019-08-20 17:09:08 -0400

    login form

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Route, Link } from 'react-router-dom'
 import HomePage from './components/screens/HomePage/HomePage'
 import Project from './components/screens/Project/Project'
 import SignUpForm from './components/screens/SignUpForm'
+import LoginForm from './components/screens/LoginForm';
 import Axios from 'axios'
 
 export const apiCall = Axios.create({ baseURL: "http://localhost:8001" })
@@ -16,7 +17,7 @@ export default function App() {
         <Link to="/">Homepage</Link>
         <Link to="/project/2">Project</Link>
         <Link to="/user/signup">Sign Up</Link>
-
+        <Link to="/user/login">Login</Link>
       </nav>
       <main>
         <Route
@@ -30,6 +31,10 @@ export default function App() {
         <Route 
           path="/user/signup"
           component={SignUpForm}
+        />
+        <Route 
+          path="/user/login"
+          component={LoginForm}
         />
       </main>
     </div>
